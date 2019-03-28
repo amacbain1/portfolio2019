01bfc64701a0c0dfdfb7d81fb06ab1ca56e19feb
Amber Stumpf <aiyemacbain@gmail.com>
2019-03-27 17:14:19 -0700

    Adjusts home screen for small mobile

--- src/components/home.js
+++ src/components/home.js
@@ -9,26 +9,28 @@ class Home extends Component {
     return (
       <section id='home'>
         <div className='container'>
-          <div className='card-columns mx-auto'>
+          <div className='card-columns'>
 
 
-            <div className='card mx-2 card-img card-main'>
+            <div className='card card-img card-main mx-auto'>
 
             </div>
 
-            <div className='card mx-2 card-skills card-main text-center'>
+            <div className='card card-skills card-main text-center mx-auto'>
               <h2 className='card-title text-white-50 m-3'>Front End Web Development</h2>
-              <p className='card-body text-white-50'>HTML, CSS, Bootstrap, JavaScript, React, Git<br /><hr /><em style={{ fontFamily: 'Cinzel Decorative' }}>Amber</em> loves to collaborate and wishes she could install Git to manage her daily life.</p>
+              <p className='card-body'>HTML, CSS, Bootstrap, JavaScript, React, Git</p>
+              <hr />
+              <p> <em style={{ fontFamily: 'Cinzel Decorative' }}>Amber</em> loves to collaborate and wishes she could install Git to manage her daily life.</p>
 
             </div>
 
-            <div className='card mx-2 card-name card-main'>
+            <div className='card card-name card-main'>
               <div className='card-body px-3 py-1'>
                 <h1 className='name'>
                   <hr />
 
-                  <p className='first'>Amber</p><br />
-                  <p className='middle'>Macbain</p><br />
+                  <p className='first'>Amber </p><br />
+                  <p className='middle'>Macbain </p><br />
                   <p className='last'>Stumpf</p>
                   <hr />
 
