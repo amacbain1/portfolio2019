14b2f4d4d38fc0cc03b3c9343a95ef1a1cd92b59
Amber Stumpf <aiyemacbain@gmail.com>
2019-03-29 10:16:54 -0700

    Fixes font-size w/media queries issue

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,20 +10,22 @@ class Contact extends Component {
             <section id='contact'>
                 <div className='container'>
                     <div className='card-columns d-flex justify-content-between text-center'>
-                        <div className='card contact-card m-5 p-2'>
-                            <h4 className='name'>Amber Macbain Stumpf</h4>
-                            <hr className='dark' />
-                            <p>Reach out to me at<br />aiyemacbain@gmail.com</p>
-                        </div>
-                        <div className='card contact-card m-5 p-3'>
-                            <hr />
-                            <p>
-                                <a href='https://www.linkedin.com/in/amber-macbain-0b8422170/'>LinkedIn </a>
-                                <a href='https://github.com/amacbain1'>GitHub </a>
-                                <a href='http://www.exploresocalifornia.com/contact.html'>ExploreSoCalifornia </a>
-                                <a href='https://www.ledkyb.com/'>Ledkyb Studios</a>
-                            </p>
-                            <hr />
+                        <div className='container align-items-center'>
+                            <div className='card contact-card p-2 m-2'>
+                                <h4 className='name'>Amber Macbain Stumpf</h4>
+                                <hr className='dark' />
+                                <p>Reach out to me at aiyemacbain@gmail.com</p>
+                            </div>
+                            <div className='card contact-card p-2 m-2'>
+                                <hr />
+                                <p>
+                                    <a href='https://www.linkedin.com/in/amber-macbain-0b8422170/'>LinkedIn </a>
+                                    <a href='https://github.com/amacbain1'>GitHub </a>
+                                    <a href='http://www.exploresocalifornia.com/contact.html'>ExploreSoCalifornia </a>
+                                    <a href='https://www.ledkyb.com/'>Ledkyb Studios</a>
+                                </p>
+                                <hr />
+                            </div>
                         </div>
                     </div>
                 </div>
